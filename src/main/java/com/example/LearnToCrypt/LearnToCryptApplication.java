cee0dd4c33e2b5e53c7cc4618f2628b8d07148b7
hpamnani <harshpam1993@gmail.com>
2019-06-02 11:38:20 -0300

    Spring Boot Starter Package

---
+++ src/main/java/com/example/LearnToCrypt/LearnToCryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnToCrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToCryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnToCryptApplication.class, args);
+	}
+
+}
