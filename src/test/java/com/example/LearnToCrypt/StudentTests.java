feaf090defec6426c1e9fb361a40d5d6b01831db
Aman Arya <aman.arya@dal.ca>
2019-06-03 00:26:17 -0300

    [UnitTest] Added a unit test

--- src/test/java/com/example/LearnToCrypt/StudentTests.java
+++ src/test/java/com/example/LearnToCrypt/StudentTests.java
@@ -11,27 +11,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class StudentTests {
 
-	@Test
-	public void getStudentNameTest() {
-		Student s1 = new Student("harsh", "hr340096@dal.ca");
-		assertEquals("getStudentNameTest", "harsh", s1.getName());
-	}
-	
-	@Test
-	public void getStudentEmailTest() {
-		Student s2 = new Student("tony", "tony123@dal.ca");
-		assertEquals("getStudentEmailTest", "tony123@dal.ca", s2.getEmail());
-	}
-	
-	@Test
-	public void newTest() {
-		Student s3 = new Student("aman", "abbbcdd@dal.ca");
-		assertEquals("getStudentEmailTest", "abbbcdd@dal.ca", s3.getEmail());
-	}
-	
-	@Test
-	public void newTest2() {
-		Student s3 = new Student("aman", "fail@dal.ca");
-		assertEquals("getStudentNameTest", "aman", s3.getName());
-	}
+    @Test
+    public void getStudentNameTest() {
+        Student s1 = new Student("harsh", "hr340096@dal.ca");
+        assertEquals("getStudentNameTest", "harsh", s1.getName());
+    }
+
+    @Test
+    public void getStudentEmailTest() {
+        Student s2 = new Student("tony", "tony123@dal.ca");
+        assertEquals("getStudentEmailTest", "tony123@dal.ca", s2.getEmail());
+    }
+
+    @Test
+    public void newTest() {
+        Student s3 = new Student("aman", "abbbcdd@dal.ca");
+        assertEquals("getStudentEmailTest", "abbbcdd@dal.ca", s3.getEmail());
+    }
+
+    @Test
+    public void newTest2() {
+        Student s3 = new Student("aman", "fail@dal.ca");
+        assertEquals("getStudentNameTest", "aman", s3.getName());
+    }
+
+    @Test
+    public void newTest3() {
+        Student s3 = new Student("arya", "arya@dal.ca");
+        assertEquals("getStudentNameTest", "arya", s3.getName());
+    }
 }
