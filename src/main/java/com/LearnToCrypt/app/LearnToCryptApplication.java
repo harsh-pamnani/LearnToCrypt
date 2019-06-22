bc979b8048290e73e3103fd3ba86302e6ff60095
Shengtian Tang <sh625730@dal.ca>
2019-06-22 09:13:32 -0300

    log4j2 test 1 done

--- src/main/java/com/LearnToCrypt/app/LearnToCryptApplication.java
+++ src/main/java/com/LearnToCrypt/app/LearnToCryptApplication.java
@@ -1,6 +1,8 @@
 package com.LearnToCrypt.app;
 
 import com.LearnToCrypt.Algorithm.AlgorithmController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,7 +23,14 @@ import com.LearnToCrypt.Dashboard.DashboardController;
 })
 public class LearnToCryptApplication {
 
+	private static final Logger logger = LogManager.getLogger(LearnToCryptApplication.class);
+
 	public static void main(String[] args) {
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
 		SpringApplication.run(LearnToCryptApplication.class, args);
 	}
 
