b20430470c2c835d6df403017eb828fa88e6b5a0
Aman Arya <aman.arya@dal.ca>
2019-06-23 23:32:10 -0300

    [Test] Updated test cases

---
+++ src/test/java/com/LearnToCrypt/DAO/ProfileUpdateDAOTest.java
@@ -0,0 +1,66 @@
+package com.LearnToCrypt.DAO;
+
+import com.LearnToCrypt.BusinessModels.BusinessModelAbstractFactory;
+import com.LearnToCrypt.BusinessModels.User;
+import com.LearnToCrypt.HashingAlgorithm.MD5;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProfileUpdateDAOTest {
+	BusinessModelAbstractFactory modelAbstractFactory;
+	IDAOAbstractFactory abstractFactory;
+	INameSetterDAO nameSetterDAO;
+	IPasswordUpdaterDAO passwordUpdaterDAO;
+	IUserDAO userDAO;
+	User user;
+	MD5 md5;
+
+	public ProfileUpdateDAOTest() {
+		abstractFactory = new DAOAbstractFactory();
+		userDAO = abstractFactory.createUserDAO();
+		md5 = new MD5();
+		nameSetterDAO = abstractFactory.createNameSetterDAO();
+		passwordUpdaterDAO = abstractFactory.createPasswordSetterDAO();
+		modelAbstractFactory = new BusinessModelAbstractFactory();
+		user = modelAbstractFactory.createUser();
+		user.setEmail("test@testing.com");
+		user.setPassword("Test@123");
+		user.setName("Test");
+		user.setRole("Student");
+		if(!userDAO.isUserRegistered(user)) {
+			userDAO.createUser(user);
+		}
+	}
+
+	@Test
+	public void testSetPassword() {
+		passwordUpdaterDAO.setPassword(user.getEmail(), "Test@1234");
+		String hash = md5.generateMD5HashValue("Test@1234");
+		User testUser = userDAO.getUser(user.getEmail());
+		passwordUpdaterDAO.setPassword(user.getEmail(), user.getPassword());
+		assertEquals(testUser.getPassword(), hash);
+	}
+
+	@Test
+	public void testSetName() {
+		nameSetterDAO.setName(user.getEmail(), "Test Name");
+		User testUser = userDAO.getUser(user.getEmail());
+		nameSetterDAO.setName(user.getEmail(), user.getName());
+		assertEquals("Test Name", testUser.getName());
+	}
+
+	@Test
+	public void testSetResetToken() {
+		passwordUpdaterDAO.setResetToken(user.getEmail(), "test-token");
+		String email = passwordUpdaterDAO.getEmailFromToken("test-token");
+		assertEquals(user.getEmail(), email);
+	}
+
+	@Test
+	public void testGetEmailFromToken() {
+		passwordUpdaterDAO.setResetToken(user.getEmail(), "test-token");
+		String email = passwordUpdaterDAO.getEmailFromToken("test-token");
+		assertEquals(user.getEmail(), email);
+	}
+}
--- src/test/java/com/LearnToCrypt/Profile/UserProfileTest.java
+++ src/test/java/com/LearnToCrypt/Profile/UserProfileTest.java
@@ -1,29 +1,46 @@
 package com.LearnToCrypt.Profile;
 
+import com.LearnToCrypt.BusinessModels.BusinessModelAbstractFactory;
+import com.LearnToCrypt.BusinessModels.User;
+import com.LearnToCrypt.DAO.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UserProfileTest {
 
     IUserProfileBridge profile;
+    BusinessModelAbstractFactory businessModelAbstractFactory;
+    IDAOAbstractFactory abstractFactory;
+    IUserDAO userDAO;
+    User user;
 
     public UserProfileTest() {
-        //TODO: Test Setup
-        profile = new UserProfileMock("Hali Fax", "Halifax@ns.com", "Student");
+        businessModelAbstractFactory = new BusinessModelAbstractFactory();
+        abstractFactory = new DAOAbstractFactory();
+        userDAO = abstractFactory.createUserDAO();
+        user = businessModelAbstractFactory.createUser();
+        user.setEmail("test@profile.com");
+        user.setPassword("Profile@123");
+        user.setName("Profile");
+        user.setRole("Student");
+        if(!userDAO.isUserRegistered(user)) {
+            userDAO.createUser(user);
+        }
+        profile = new UserProfile(user.getEmail());
     }
 
     @Test
     public void testGetUserName() {
-        assertEquals("Hali Fax", profile.getUserName());
+        assertEquals(user.getName(), profile.getUserName());
     }
 
     @Test
     public void testGetEmail() {
-        assertEquals("Halifax@ns.com", profile.getEmail());
+        assertEquals(user.getEmail(), profile.getEmail());
     }
 
     @Test
     public void testGetRole() {
-        assertEquals("Student", profile.getRole());
+        assertEquals(user.getRole(), profile.getRole());
     }
 }
