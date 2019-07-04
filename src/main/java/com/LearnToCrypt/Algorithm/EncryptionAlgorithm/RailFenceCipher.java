e33dc6d47b075540df5863f926bccf6bb919ef80
hpamnani <harshpam1993@gmail.com>
2019-07-04 03:35:51 -0300

    Rail Fence Cipher added and some modification in algorithm/dashboard page

---
+++ src/main/java/com/LearnToCrypt/Algorithm/EncryptionAlgorithm/RailFenceCipher.java
@@ -0,0 +1,62 @@
+package com.LearnToCrypt.Algorithm.EncryptionAlgorithm;
+
+public class RailFenceCipher implements IEncryptionAlgorithm {
+
+	private String result = "Cipher Text: ";
+	private String steps = "";
+	private String plaintext = null;
+
+	@Override
+	public String encode(String key, String plaintext) {
+		this.plaintext = "Plain Text: " + plaintext;
+
+		int keyToEncrypt = Integer.parseInt(key);
+
+		plaintext = plaintext.replace(" ", "%");
+		int plaintextLength = plaintext.length();
+		
+		int reminder = 0;
+		if(plaintextLength < keyToEncrypt) {
+			reminder = keyToEncrypt - plaintextLength;
+		} else {
+			reminder = plaintextLength % keyToEncrypt;
+		}
+		
+		for (int i = 0; i < reminder; i++) {
+			plaintext += "%";
+		}
+
+		plaintextLength = plaintext.length();
+		int matrixWidth = plaintextLength / keyToEncrypt;
+		int matrixHeight = keyToEncrypt;
+		
+		char ciphertextMatrix[][] = new char[matrixHeight][matrixWidth];
+		
+		int index = 0;
+		for (int i = 0; i < matrixWidth; i++) {
+			for (int j = 0; j < keyToEncrypt; j++) {
+				ciphertextMatrix[j][i] = plaintext.charAt(index++);	
+			}
+		}
+		
+		String ciphertext = "";
+		for (int i = 0; i < keyToEncrypt; i++) {
+			for (int j = 0; j < matrixWidth; j++) {
+				ciphertext += ciphertextMatrix[i][j];
+			}
+		}
+
+		result += (ciphertext + "\n\nNOTE: % represents space");
+		return "";
+	}
+
+	@Override
+	public String getResult() {
+		return plaintext + "\n" + result;
+	}
+
+	@Override
+	public String getSteps() {
+		return steps;
+	}
+}
